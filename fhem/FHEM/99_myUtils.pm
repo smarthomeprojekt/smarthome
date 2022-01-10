08ebe8cc14fc4c7b966b809cc1dca0ea3e60b5aa
Lukas <wi19270@lehre.dhbw-stuttgart.de>
2022-01-10 17:30:16 +0100

    .

--- fhem/FHEM/99_myUtils.pm
+++ fhem/FHEM/99_myUtils.pm
@@ -8,46 +8,4 @@ myUtils_Initialize($$)
 my ($hash) = @_;
 }
 
-
-
-
-sub Kalenderstart ($)
-{
-    my ($Ereignis) = @_;
-    my @Ereignisarray = split(/.*:\s/, $Ereignis);
-    my $Ereignisteil1 = $Ereignisarray[1];
-    my @uids = split(/;/, $Ereignisteil1);
-    
-    foreach my $uid (@uids) {
-        my $Kalendertext = fhem("get MuelltonnenKalender summary $uid");
-        if ($Kalendertext =~ /Biotonne/) {
-            fhem("set Bio_Tonne ja");
-        };
-        if ($Kalendertext =~ /Restmüll 2-wöchentlich/) {
-            fhem("set Restmuell_Tonne ja");
-        };
-        if ($Kalendertext =~ /Papiertonne/) {
-            fhem("set Papier_Tonne ja");
-        };
-    };
-}
-sub Kalenderende ($) {
-    my ($Ereignis) = @_;
-    my @Ereignisarray = split(/.*:\s/, $Ereignis);
-    my $Ereignisteil1 = $Ereignisarray[1];
-    my @uids = split(/;/, $Ereignisteil1);
-    
-    foreach my $uid (@uids) {
-        my $Kalendertext = fhem("get MuelltonnenKalender summary $uid");
-        if ($Kalendertext =~ /Biotonne/) {
-            fhem("set Bio_Tonne nein");
-        };
-        if ($Kalendertext =~ /Restmüll 2-wöchentlich/) {
-            fhem("set Restmuell_Tonne nein");
-        };
-        if ($Kalendertext =~ /Papiertonne/) {
-            fhem("set Papier_Tonne nein");
-        };
-    };
-}
 1;
